20f48c55327326f706a27198741244a544dcdebd
IramMehmooda <iram4011@gmail.com>
2018-03-26 10:02:15 -0500

    initial code- Hibernate

---
+++ script.sql
@@ -0,0 +1,15 @@
+create table Employee(id int NOT NULL,
+name varchar(255) NOT NULL,
+department varchar(255) NOT NULL,
+PRIMARY KEY(id));
+
+create table Manager(
+id int NOT NULL,
+name varchar(255),
+PRIMARY KEY(id)
+);
+
+create table Task()
+
+
+
